d99a7c7c63e24dd7e42445b8464ee2a9bfdaa5e5
Emily Batista“git config --global user.name Emily <emilybatista.programmer@hotmail.com>
2026-05-30 12:59:33 +0100

    Meus ajustes na Unity

---
+++ Assets/Game/Scripts/Exit.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Game/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void ExitGame()
+    {
+        //Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
